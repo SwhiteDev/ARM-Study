4578ac323f57ce6ee4f9f2a174a854ac9bd76032
Swhite <1779011982@qq.com>
2017-03-23 18:49:19 +0800

    sdram

--- export_config.sh
+++ export_config.sh
@@ -1,1 +1,1 @@
-export CLASSPATH="$PATH:~/ELinux/tools_/tools/gcc-3.4.5-glibc-2.3.6/bin/"
+export PATH="$PATH:/home/swhite/ELinux/tools_/tools/gcc-3.4.5-glibc-2.3.6/libexec/gcc/arm-linux/3.4.5/:/home/swhite/Elinux/tools_/tools/gcc-3.4.5-glibc-2.3.6/bin/"
